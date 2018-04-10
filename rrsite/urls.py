20ca1df84ddabca59ba2d08569e8434ed5f9d4e1
songhan1314lh <983514547@qq.com>
2018-04-10 15:31:54 +0800

    login前端界面实现

--- rrsite/urls.py
+++ rrsite/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = "rrsite"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login),
+    path('register', views.register)
 ]
--- rrsite/views.py
+++ rrsite/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("rrsite app index")
+
+
+def login(request):
+    return render(request, 'rrsite/login.html')
+
+
+def register(request):
+    return render(request, 'rrsite/register.html')
